bde5057000baac81033e93e1c990c6d3f2f72b79
Aditya Tomar✨ <98268205+CodeX-Addy@users.noreply.github.com>
2024-04-20 13:39:59 +0000

    echo command

---
+++ Intro to bash/echo.sh
@@ -0,0 +1,3 @@
+echo "Hello, World\n\n" #---> print as it is in next line
+echo -e "Hello, World\n\n" #--> print hello world along with two blank lines
+echo -n "Hello, World\n\n" #--> print as it is without any line
